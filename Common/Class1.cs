943086f13cd50f0c60515cf2a495de124490bc28
releaseStars <809117178@qq.com>
2024-01-03 23:41:30 +0800

    提交字符串扩展方法

--- Common/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Common
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Common/StringExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Common
+{
+    /// <summary>
+    /// 字符串扩展方法类
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 是否为空
+        /// </summary>
+        /// <param name="str">扩展对象</param>
+        /// <returns>为空 true,否则 false</returns>
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        /// <summary>
+        /// 是否为空(包括空格)
+        /// </summary>
+        /// <param name="str">扩展对象</param>
+        /// <returns>为空 true,否则 false</returns>
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+
+        /// <summary>
+        /// 反转字符串
+        /// </summary>
+        /// <param name="str">扩展对象</param>
+        /// <returns>反转后的字符串</returns>
+        public static string Reverse(this string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
